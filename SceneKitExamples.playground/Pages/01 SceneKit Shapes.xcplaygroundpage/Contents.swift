00da954eaf4fd141df243e845a3015387a278000
Eric Internicola <intere@gmail.com>
2017-01-29 06:34:40 -0700

    Annotated the SceneKit examples that I have so far.

--- SceneKitExamples.playground/Pages/01 SceneKit Shapes.xcplaygroundpage/Contents.swift
+++ SceneKitExamples.playground/Pages/01 SceneKit Shapes.xcplaygroundpage/Contents.swift
@@ -1,23 +1,37 @@
-//: Playground - noun: a place where people can play
+/*:
+ # 01 SceneKit examples
+ This example will create a random geometrical shape.
+ */
 
 import SceneKit
 import PlaygroundSupport
+
+/*:
+ ---
+ ### Create the SceneKit View
+ */
 
 let view = SCNView(frame: NSRect(x: 0, y: 0, width: 1024, height: 768))
 view.showsStatistics = true
 view.allowsCameraControl = true
 view.autoenablesDefaultLighting = true
-
 PlaygroundPage.current.liveView = view
 
-
+/*:
+ ---
+ ### Create the Scene
+ */
 let scene = SCNScene()
+view.scene = scene
 
+/*:
+ ---
+ ### Add a random shape of random color
+ */
 let shape = ShapeType.random
 let node = SCNNode(geometry: shape.geometry)
 node.geometry?.materials.first?.diffuse.contents = NSColor.random
 
 scene.rootNode.addChildNode(node)
 
-view.scene = scene
-
+//: [Next](@next)
--- SceneKitExamples.playground/Pages/02 Imported Model.xcplaygroundpage/Contents.swift
+++ SceneKitExamples.playground/Pages/02 Imported Model.xcplaygroundpage/Contents.swift
@@ -1,17 +1,33 @@
 //: [Previous](@previous)
 
+/*:
+ # 02 Imported Model
+ This example will create a random geometrical shape.
+ */
+
 import SceneKit
 import PlaygroundSupport
 
+/*:
+ ---
+ ### Create the SceneKit View
+ */
 let view = SCNView(frame: NSRect(x: 0, y: 0, width: 1024, height: 768))
 view.showsStatistics = true
 view.allowsCameraControl = true
-
-
 PlaygroundPage.current.liveView = view
 
-
+/*:
+ ---
+ ### Import the model (Scene)
+ */
 let scene = SCNScene(named: "tardis.dae")
+view.scene = scene
+
+/*:
+ ---
+ ### Create and add 3 lights
+ */
 let ambient1 = SCNNode()
 ambient1.light = SCNLight()
 ambient1.light?.type = .ambient
@@ -30,12 +46,17 @@ spot2.light?.type = .ambient
 spot2.position = SCNVector3(x: 5, y: 5, z: 9)
 scene?.rootNode.addChildNode(spot2)
 
+/*:
+ ---
+ ### Get a reference to the Tardis Node
+ */
 let tardis = scene?.rootNode.childNode(withName: "Tardis", recursively: false)
 
-view.scene = scene
-
+/*:
+ ---
+ ### Create a rotate action and run it on the Tardis
+ */
 let rotate = SCNAction.rotateBy(x: 0, y: 0, z: CGFloat(45 / M_PI), duration: 3)
-
 tardis?.runAction(rotate)
 
 
